ea00f0d70ba6121c67c427c86331f7dff35df91e
DantesDesmond <88592489+DantesDesmond@users.noreply.github.com>
2021-09-04 23:05:02 -0400

    Add files via upload

---
+++ ArbolBinario/Arbol.cs
@@ -0,0 +1,96 @@
+﻿namespace ArbolBinario
+{
+    //Metodo EstaVacio -- si el nodo raiz es null, entonces el arbol esta vacio
+    //Metodo AgregarNodo -- agrega nodo en la raiz o ramas segun sea necesario
+    //metodo ObtenerCantidadNodos -- retorna conteo de nodos en el arbol
+    //metodo ObtenerCantidadHojas -- retorna conteo de la cantidad de hojas
+    //metodo VerificarSiEsHoja(int value) -- verifica si el nodo existe, si existe, retorna verdadero si los hijos del nodo son null
+
+    public class Arbol
+    {
+        private Nodo Raiz;
+        private int CantidadNodos;
+        private int CantidadHojas;
+
+        public bool EstaVacio()
+        {
+            return Raiz == null;
+        }
+
+        public void AgregarNodo(int valor)
+        {
+            AgregarNodo(ref Raiz, valor);
+        }
+
+        private void AgregarNodo(ref Nodo nodoActual, int valor)
+        {
+            if (nodoActual == null) //si la raiz es igual a null, significa que esta vacio y se inserta el valor(dato) como raiz del nodo
+            {
+                nodoActual = new Nodo { Dato = valor, HijoIzq = null, HijoDer = null };
+            }
+            else
+            {
+                //si el arbol no esta vacio, debemos verificar el valor de la raiz para insertar a la izquierda o derecha
+                //si el valor a insertar es menor que el valor raiz, se inserta por la izquierda.
+                //si el valor a insertar es mayor que el valor raiz, se inserta por la derecha.
+                int valorNodo = nodoActual.Dato;
+                if (valor < valorNodo)
+                {
+                    AgregarNodo(ref nodoActual.HijoIzq, valor);
+                }
+                else
+                {
+                    AgregarNodo(ref nodoActual.HijoDer, valor);
+                }
+            }
+        }
+
+        public int ObtenerCantidadNodos()
+        {
+            CantidadNodos = 0;
+
+            ContarNodos(ref Raiz);
+
+            return CantidadNodos;
+        }
+
+        private void ContarNodos(ref Nodo nodoActual)
+        {
+            if (nodoActual != null)
+            {
+                CantidadNodos++;
+                ContarNodos(ref nodoActual.HijoIzq);
+                ContarNodos(ref nodoActual.HijoDer);
+            }
+        }
+
+        public int ObtenerCantidadHojas()
+        {
+            CantidadHojas = 0;
+            ContarHojas(Raiz);
+            return CantidadHojas;
+        }
+
+        private void ContarHojas(Nodo nodoActual)
+        {
+            if (nodoActual != null && (nodoActual.HijoIzq == null && nodoActual.HijoDer == null))
+            {
+                CantidadHojas++;
+            }
+
+            if (nodoActual != null)
+            {
+                ContarHojas(nodoActual.HijoIzq);
+                ContarHojas(nodoActual.HijoDer);
+            }
+        }
+
+    }
+
+    public class Nodo
+    {
+        public int Dato;
+        public Nodo HijoDer;
+        public Nodo HijoIzq;
+    }
+}
---
+++ ArbolBinario/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Arbol arbol = new Arbol();
+
+            Console.WriteLine("El arbol esta vacio? : " + arbol.EstaVacio());
+            arbol.AgregarNodo(8);
+            arbol.AgregarNodo(6);
+            arbol.AgregarNodo(9);
+            arbol.AgregarNodo(3);
+            arbol.AgregarNodo(7);
+            arbol.AgregarNodo(14);
+            Console.WriteLine("El arbol esta vacio? : " + arbol.EstaVacio());
+            Console.WriteLine("Cantidad de nodos en el arbol: "+arbol.ObtenerCantidadNodos());
+            Console.WriteLine("Cantidad de hojas en el arbol: " + arbol.ObtenerCantidadHojas());
+
+            Console.ReadLine();
+        }
+    }
+}
